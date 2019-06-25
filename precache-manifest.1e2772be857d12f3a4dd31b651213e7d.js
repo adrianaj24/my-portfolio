9cb77da012a37572c7359802de8d2b72d608b236
Adriana Jimenez <ajimenez0024@gmail.com>
2019-06-25 15:43:17 -0400

    Updates

---
+++ precache-manifest.1e2772be857d12f3a4dd31b651213e7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d6874fde7ecb3f07643b60b3bd0a79",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "b726a2ad008baa70ce92",
+    "url": "/my-portfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "87c85b99f7ae2b8af7a7",
+    "url": "/my-portfolio/static/css/main.52632008.chunk.css"
+  },
+  {
+    "revision": "b726a2ad008baa70ce92",
+    "url": "/my-portfolio/static/js/2.c4341ecc.chunk.js"
+  },
+  {
+    "revision": "87c85b99f7ae2b8af7a7",
+    "url": "/my-portfolio/static/js/main.10a73308.chunk.js"
+  },
+  {
+    "revision": "f2d15970f74596a69ca7",
+    "url": "/my-portfolio/static/js/runtime~main.674e5ed7.js"
+  }
+]);
